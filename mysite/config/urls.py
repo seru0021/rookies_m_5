100d2554406c3ba5a5dd88c51678ebfa0e9983f8
DongSik7271 <166671632+DongSik7271@users.noreply.github.com>
2024-04-21 20:53:10 +0900

    Update urls.py

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from pybo import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),  # 'admin' 네임스페이스 추가
+    path('admin/', admin.site.urls),  # 'admin' 네임스페이스 추가
     path('', include('pybo.urls')),
-    path('common/', include('common.urls'), name='common'),  # 'common' 네임스페이스 추가
+    path('common/', include('common.urls')),
     path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
