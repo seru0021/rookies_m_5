722248832677978e9e3e22900ecaa79b27c13c0c
DongSik7271 <166671632+DongSik7271@users.noreply.github.com>
2024-04-21 20:44:14 +0900

    Update models.py

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import User
 
+
+class UserProfile(models.Model):
+    password = models.CharField(max_length=128, null=True)
+    
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
@@ -18,25 +22,32 @@ class Post(models.Model):
         return self.title
 
 # 사용자 매니저 클래스 정의
-class CustomUserManager(BaseUserManager):
-    def create_user(self, user_id, nickname, password=None):
-        if not user_id:
-            raise ValueError('The user_id must be set')
-        
-        user = self.model(
-            user_id=user_id,
-            nickname=nickname,
-        )
+class CustomUser(AbstractBaseUser):
+    user_id = models.CharField(max_length=20, unique=True)
+    nickname = models.CharField(max_length=20)
+    email = models.EmailField(max_length=30, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+    objects = CustomUserManager()
 
-    def create_superuser(self, user_id, nickname, password=None):
+    user_id = 'user_id'
+    REQUIRED_FIELDS = ['nickname']
+
+    def __str__(self):
+        return self.user_id
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def create_superuser(self, user_id, nickname):
         user = self.create_user(
             user_id=user_id,
             nickname=nickname,
-            password=password,
         )
         user.is_admin = True
         user.is_staff = True
