b39ec17e854275e3fc9ca04003bb292bc740a006
KimGoEun <56550401+seru0021@users.noreply.github.com>
2024-04-22 00:00:52 +0900

    Update views.py

--- mysite/common/views.py
+++ mysite/common/views.py
@@ -1,7 +1,19 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from common.forms import UserForm
+from django.contrib.auth import logout
+from django.conf import settings
+from django.utils import timezone
+
+class CustomLoginView(LoginView):
+    template_name = 'common/login.html'  # 로그인 페이지 템플릿 설정
+
+    def dispatch(self, request, *args, **kwargs):
+        # 세션 만료 시간 설정
+        request.session.set_expiry(settings.SESSION_COOKIE_AGE)
+        return super().dispatch(request, *args, **kwargs)
 
 def logout_view(request):
     logout(request)
@@ -24,3 +36,19 @@ def signup(request):
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
+
+# 로그인 후 일정 시간 동안 활동하지 않으면 세션을 종료하는 기능
+class CustomLoginRequiredMixin(LoginRequiredMixin):
+    idle_timeout = settings.SESSION_IDLE_TIMEOUT
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            last_activity = request.session.get('last_activity')
+            if last_activity:
+                elapsed_time = timezone.now() - last_activity
+                if elapsed_time.total_seconds() > self.idle_timeout:
+                    logout(request)
+                    messages.warning(request, '세션이 만료되었습니다. 다시 로그인해주세요.')
+                    return redirect('common:login')
+        request.session['last_activity'] = timezone.now()
+        return super().dispatch(request, *args, **kwargs)
